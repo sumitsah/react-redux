c0f56db161aa6688087b452f2e4a106dcf0798ed
sumit.sahu <sumit.sahu@clariontechnologies.co.in>
2020-11-19 18:10:15 +0530

    Dispatching actions and adding subscriptions

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,20 @@ const initialState = {
 
 //Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === 'ADD_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
+
     return state;
 };
 
@@ -14,7 +28,13 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+//Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
 
 //Displaying Action 
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
-//Subscription
